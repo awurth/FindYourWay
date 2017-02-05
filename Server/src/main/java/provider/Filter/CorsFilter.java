2d848d8e5b258ec32671e2baa352f6341e03a3d2
Xavier Chopin <xchopin54@gmail.com>
2017-02-05 19:51:26 +0100

    Added CORS

---
+++ Server/src/main/java/provider/Filter/CorsFilter.java
@@ -0,0 +1,39 @@
+package provider.Filter;
+
+import javax.ws.rs.container.*;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+
+@PreMatching
+@Provider
+public class CorsFilter implements ContainerRequestFilter, ContainerResponseFilter {
+
+    private final static String METHODS = "GET,POST,PUT,DELETE,OPTIONS,HEAD";
+    private final static String HEADERS = "origin,content-type,accept,authorization";
+    private final static int TIME = 15 * 60;
+
+    @Override
+    public void filter(ContainerRequestContext requestCtx) throws IOException {
+        if (requestCtx.getRequest().getMethod().equals("OPTIONS"))
+            requestCtx.abortWith(Response.status(Response.Status.OK).build());
+    }
+
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+        try {
+            responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
+            responseContext.getHeaders().add("Access-Control-Allow-Headers", createHeader(requestContext));
+            responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
+            responseContext.getHeaders().add("Access-Control-Allow-Methods", METHODS);
+            responseContext.getHeaders().add("Access-Control-Max-Age", TIME);
+        } catch (Exception e) {
+            throw new SecurityException();
+        }
+    }
+
+    private String createHeader(ContainerRequestContext requestContext) {
+        String headers = requestContext.getHeaderString("Access-Control-Request-Headers");
+        return headers == null ? HEADERS : headers;
+    }
+}
