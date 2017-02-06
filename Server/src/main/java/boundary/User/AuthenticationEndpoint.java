276ce30750abc5b2d092f8b3baa1665b1e30ae32
Xavier Chopin <xchopin54@gmail.com>
2017-02-06 23:24:10 +0100

    Changed package name into User

---
+++ Server/src/main/java/boundary/User/AuthenticationEndpoint.java
@@ -0,0 +1,86 @@
+package boundary.User;
+
+import control.KeyGenerator;
+
+import entity.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.mindrot.jbcrypt.BCrypt;
+
+import javax.ejb.EJB;
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+import static javax.ws.rs.core.HttpHeaders.AUTHORIZATION;
+
+
+@Path("/authentication")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class AuthenticationEndpoint {
+
+    @Inject
+    private KeyGenerator keyGenerator;
+
+    @EJB
+    private UserResource accountResource;
+
+    @Context
+    private UriInfo uriInfo;
+
+    @POST
+
+    public Response authenticateUser(User user) {
+        try {
+            authenticate(user.getEmail(), user.getPassword());
+            return Response.ok().header(AUTHORIZATION, "Bearer " + issueToken(user.getEmail())).build();
+        } catch (Exception e) {
+            return Response.status(Response.Status.UNAUTHORIZED).type("text/plain").entity("Invalid credentials").build();
+        }
+    }
+
+    /**
+     * Authenticate against a LDAP
+     * @param email
+     * @param password
+     * @throws NotAuthorizedException if the credentials are invalid
+     */
+    private void authenticate(String email, String password) throws NotAuthorizedException {
+        User user = accountResource.findByEmail(email);
+
+        if (user == null && !BCrypt.checkpw(password,user.getPassword()))
+            throw new SecurityException("Email address or password is invalid");
+    }
+
+    /**
+     * Helper method that converts a Date for a localDateTime given
+     * @param localDateTime to convert
+     * @return Date
+     */
+    private Date toDate(LocalDateTime localDateTime) {
+        return Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
+    }
+
+    /**
+     * Method that issues a JWT token
+     * @param email of the associated user
+     * @return the issued token
+     */
+    private String issueToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuer(uriInfo.getAbsolutePath().toString())
+                .setIssuedAt(new Date())
+                .setExpiration(toDate(LocalDateTime.now().plusMinutes(15L)))
+                .signWith(SignatureAlgorithm.HS512, keyGenerator.generateKey())
+                .compact();
+    }
+}
---
+++ Server/src/main/java/boundary/User/UserRepresentation.java
@@ -0,0 +1,61 @@
+package boundary.User;
+
+import control.PasswordManagement;
+import entity.User;
+import entity.UserRole;
+import provider.Secured;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
+import java.util.List;
+
+@Path("/user")
+@Stateless
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class UserRepresentation {
+
+    @EJB
+    UserResource userResource;
+
+    @GET
+    @Secured({UserRole.ADMIN})
+    @Path("/{email}")
+    public Response get(@PathParam("email") String email) {
+        User user = userResource.findByEmail(email);
+        if (user != null)
+            return Response.ok(user, MediaType.APPLICATION_JSON).build();
+        else
+            return Response.status(Response.Status.NOT_FOUND).build();
+    }
+
+    @GET
+    @Secured({UserRole.ADMIN})
+    public Response getAll(){
+        GenericEntity<List<User>> list = new GenericEntity<List<User>>(userResource.findAll()){};
+        return Response.ok(list, MediaType.APPLICATION_JSON).build();
+    }
+
+    @POST
+    @Path("/signup")
+    public Response signup(User user) {
+        if ((user.getName() == null || user.getEmail() == null || user.getPassword() == null))
+            return Response.status(Response.Status.NOT_FOUND).build();
+
+        if (userResource.findByEmail(user.getEmail()) != null)
+            return Response.status(409)
+                    .type("text/plain")
+                    .entity("This email address is already used")
+                    .build();
+
+        try {
+            userResource.insert(new User(user.getName(), user.getEmail(), PasswordManagement.digestPassword(user.getPassword())));
+            return Response.ok().build();
+        } catch (Exception e) {
+            return Response.serverError().build();
+        }
+    }
+
+}
