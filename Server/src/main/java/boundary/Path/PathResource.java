a1f6902bc8f9ebc75865fe76b96934788f54551d
xchopin <xchopin54@gmail.com>
2017-02-06 15:23:41 +0100

    Created resource file

---
+++ Server/src/main/java/boundary/Path/PathResource.java
@@ -0,0 +1,13 @@
+package boundary.Path;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+//@Stateless
+public class PathResource {
+
+    //@PersistenceContext
+   // EntityManager entityManager;
+
+}
