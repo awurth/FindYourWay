cca2e58634c13d952c14ba28d2a6b7ebc4505407
Xavier Chopin <xchopin54@gmail.com>
2017-02-07 22:01:52 +0100

    Removed author comment

--- Server/src/main/java/boundary/Score/ScoreResource.java
+++ Server/src/main/java/boundary/Score/ScoreResource.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package boundary.Score;
 
 import entity.Question;
@@ -13,10 +8,6 @@ import javax.persistence.CacheStoreMode;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
-/**
- *
- * @author Utilisateur
- */
 @Stateless
 public class ScoreResource {
     
