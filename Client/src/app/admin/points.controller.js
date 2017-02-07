12693a6b06a3bd86617b0591319a821907dabc07
Alexis Wurth <alexis.wurth57@gmail.com>
2017-02-07 11:17:47 +0100

    Added hints

--- Client/src/app/admin/points.controller.js
+++ Client/src/app/admin/points.controller.js
@@ -5,22 +5,26 @@ export default function AdminPointsController ($scope, Point) {
       id: 1,
       name: 'Point #1',
       latitude: '164564',
-      longitude: '5484646'
+      longitude: '5484646',
+      hint: 'shbhqefoenber'
     }, {
       id: 2,
       name: 'Point #2',
       latitude: '44537',
-      longitude: '453453'
+      longitude: '453453',
+      hint: 'sejtjtdjsderj'
     }, {
       id: 3,
       name: 'Point #3',
       latitude: '7687',
-      longitude: '4578'
+      longitude: '4578',
+      hint: 'qrhetjhedqj'
     }, {
       id: 4,
       name: 'Point #4',
       latitude: '786',
-      longitude: '4537'
+      longitude: '4537',
+      hint: 'ereqhrqthe'
     }
   ]
 
