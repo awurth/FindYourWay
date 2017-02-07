35c39323d84821ee16237c18c4bc96d1e6e38e28
xchopin <xchopin54@gmail.com>
2017-02-07 10:43:38 +0100

    Added insert method

--- Server/src/main/java/boundary/Point/PointResource.java
+++ Server/src/main/java/boundary/Point/PointResource.java
@@ -21,4 +21,15 @@ public class PointResource {
         return entityManager.find(Point.class, id);
     }
 
+    /**
+     * Method to insert a point into the database
+     * the id is also generated in this part
+     * @param point Point to add
+     * @return the Point added
+     */
+    public Point insert(Point point) {
+        point.generateId();
+        return entityManager.merge(point);
+    }
+
 }
