ec119a18129d72848c45ee3a3f742b9d8f7e7385
Xavier Chopin <xchopin54@gmail.com>
2017-02-07 20:53:49 +0100

    Improved security

--- Server/src/main/java/boundary/Score/ScoreRepresentation.java
+++ Server/src/main/java/boundary/Score/ScoreRepresentation.java
@@ -1,30 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package boundary.Score;
 
+import boundary.User.UserResource;
 import entity.Score;
+import entity.User;
+import entity.UserRole;
+import provider.Secured;
+
 import java.util.List;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
-import static javax.ws.rs.HttpMethod.PUT;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.GenericEntity;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
+import javax.ws.rs.core.*;
 
-/**
- *
- * @author Utilisateur
- */
 @Path("/score")
 @Stateless
 @Produces(MediaType.APPLICATION_JSON)
@@ -33,6 +26,9 @@ public class ScoreRepresentation {
     
     @EJB
     private ScoreResource scoreResource;
+
+    @EJB
+    private UserResource userResource;
     
     @GET
     public Response get() {
@@ -41,18 +37,33 @@ public class ScoreRepresentation {
     }
     
     @POST
-    public Response add(Score score) {
-        
-        //à faire les vérifs
-        
+    @Secured({UserRole.CUSTOMER})
+    public Response add(@Context SecurityContext securityContext, Score score) {
+        if(score == null)
+            return Response.status(400)
+                    .type(MediaType.TEXT_PLAIN_TYPE)
+                    .entity("You sent an empty object")
+                    .build();
+
+        if (!score.isValid())
+            return Response.status(400)
+                    .type(MediaType.TEXT_PLAIN_TYPE)
+                    .entity("One or many fields have not been filled, also be sure the score is greater than 0")
+                    .build();
+
+        User user = userResource.findByEmail(securityContext.getUserPrincipal().getName());
+
+        if (!score.getUser().equals(user))
+            return Response.status(Response.Status.UNAUTHORIZED).build();
+
         score = scoreResource.insert(score);
         
         return Response.ok(score, MediaType.APPLICATION_JSON).build();
     }
     
     @DELETE
-    public Response delete(Score score) {
-        
+    @Secured({UserRole.CUSTOMER})
+    public Response delete(@Context SecurityContext securityContext, Score score) {
         if(score == null)
             return Response.status(400)
                     .type(MediaType.TEXT_PLAIN_TYPE)
@@ -61,15 +72,19 @@ public class ScoreRepresentation {
         
         if(scoreResource.findById(score.getId()) == null) 
             return Response.noContent().build();
+
+        User user = userResource.findByEmail(securityContext.getUserPrincipal().getName());
+
+        if (!score.getUser().equals(user))
+            return Response.status(Response.Status.UNAUTHORIZED).build();
         
         scoreResource.delete(score);
-        
-        return Response.status(204).build();
+        return Response.status(Response.Status.NO_CONTENT).build();
     }
     
     @PUT
-    public Response update(Score score) {
-        
+    @Secured({UserRole.CUSTOMER})
+    public Response update(@Context SecurityContext securityContext, Score score) {
         if(score == null)
             return Response.status(400)
                     .type(MediaType.TEXT_PLAIN_TYPE)
@@ -77,13 +92,18 @@ public class ScoreRepresentation {
                     .build();
         
         score = scoreResource.findById(score.getId());
+
         if(score == null) 
             return Response.noContent().build();
+
+        if(!score.isValid())
+            return Response.status(Response.Status.NOT_FOUND).build();
+
+        User user = userResource.findByEmail(securityContext.getUserPrincipal().getName());
+
+        if (!score.getUser().equals(user))
+            return Response.status(Response.Status.UNAUTHORIZED).build();
         
-        Long l = score.getValue();
-        if((score.getUser() == null) || (l == null)) 
-            return Response.status(404).entity("Not Found").build();
-        
-        return Response.status(204).build();
+        return Response.status(Response.Status.NO_CONTENT).build();
     }
 }
