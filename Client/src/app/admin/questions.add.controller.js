cb16168f86f17c91ae9163abbe9ff1e7d5ec71f1
Alexis Wurth <alexis.wurth57@gmail.com>
2017-02-07 20:52:55 +0100

    Add question

--- Client/src/app/admin/questions.add.controller.js
+++ Client/src/app/admin/questions.add.controller.js
@@ -1,7 +1,9 @@
 
-export default function AdminAddPointController ($scope, $state, NgMap, Question) {
+export default function AdminAddQuestionController ($scope, $state, NgMap, Question) {
   $scope.googleMapsUrl = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBevGWdiDClK7DvnpjA0l96DcaIp_NqD6g'
+  $scope.points = []
   $scope.point = {}
+  $scope.editIndex = null
 
   NgMap.getMap().then((map) => {
     map.addListener('click', (e) => {
@@ -11,6 +13,41 @@ export default function AdminAddPointController ($scope, $state, NgMap, Question
       })
     })
   })
+
+  $scope.validatePoint = () => {
+    $scope.errors = {
+      name: $scope.point.name ? '' : 'The point must have a name',
+      latitude: $scope.point.latitude ? '' : 'The point must have a latitude',
+      longitude: $scope.point.longitude ? '' : 'The point must have a longitude',
+      hint: $scope.point.hint ? '' : 'The point must have a hint'
+    }
+
+    if (!$scope.errors.name && !$scope.errors.latitude && !$scope.errors.longitude && !$scope.errors.hint) {
+      if ($scope.editIndex === null) {
+        $scope.points.push($scope.point)
+      } else {
+        $scope.points[$scope.editIndex] = $scope.point
+        $scope.editIndex = null
+      }
+
+      $scope.point = {}
+    }
+  }
+
+  $scope.editPoint = (point) => {
+    $scope.point = point
+    $scope.editIndex = $scope.points.indexOf(point)
+  }
+
+  $scope.deletePoint = (point) => {
+    $scope.points.splice($scope.points.indexOf(point), 1)
+  }
+
+  $scope.submitQuestion = () => {
+    Question.save({ points: $scope.points }, () => {
+      $state.go('admin.questions')
+    })
+  }
 }
 
-AdminAddPointController.$inject = ['$scope', '$state', 'NgMap', 'Question']
+AdminAddQuestionController.$inject = ['$scope', '$state', 'NgMap', 'Question']
