b6efdfa289cda9a05302e3e2cfeaf48c34a50d16
iryu54 <corentinlabroche@gmail.com>
2017-02-10 19:47:54 +0000

    Pagination par defaut

--- Client/src/app/game/score.js
+++ Client/src/app/game/score.js
@@ -1,7 +1,13 @@
 
 export default function Score ($resource, API) {
   return $resource(API.url + 'scores/:id', { id: '@id' }, {
-    update: { method: 'PUT' }
+    update: { method: 'PUT' },
+    pagination: {
+      method: 'GET',
+      url: API.url + 'scores/pages',
+      params: {offset: '@offset', limit: '@limit'},
+      isArray: true
+    }
   })
 }
 
