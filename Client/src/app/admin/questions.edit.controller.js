2591fe2c81c60e68fbf1b98c36946fe9e8f325d1
Alexis Wurth <alexis.wurth57@gmail.com>
2017-02-10 15:35:43 +0100

    Add question hints

--- Client/src/app/admin/questions.edit.controller.js
+++ Client/src/app/admin/questions.edit.controller.js
@@ -3,6 +3,7 @@ export default function AdminEditQuestionController ($scope, $state, $stateParam
   $scope.googleMapsUrl = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBevGWdiDClK7DvnpjA0l96DcaIp_NqD6g'
   $scope.flagIconUrl = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'
   $scope.pointsCount = 6
+  $scope.finalPointHints = []
   $scope.points = []
   $scope.point = {}
   $scope.editIndex = null
@@ -26,16 +27,20 @@ export default function AdminEditQuestionController ($scope, $state, $stateParam
     })
   })
 
+  /**
+   * Validate newly created point
+   */
   $scope.validatePoint = () => {
     $scope.errors = {
       name: $scope.point.name ? '' : 'The point must have a name',
       latitude: $scope.point.latitude ? '' : 'The point must have a latitude',
       longitude: $scope.point.longitude ? '' : 'The point must have a longitude',
-      hint: $scope.point.hint ? '' : 'The point must have a hint'
+      hint: $scope.point.hint ? '' : 'The point must have a hint',
+      finalPointHint: $scope.point.finalHint ? '' : 'You must add a hint for the final point'
     }
 
     // If points are valid
-    if (!$scope.errors.name && !$scope.errors.latitude && !$scope.errors.longitude && !$scope.errors.hint) {
+    if (!$scope.errors.name && !$scope.errors.latitude && !$scope.errors.longitude && !$scope.errors.hint && !$scope.errors.finalPointHint) {
       // If we are creating a new point
       if ($scope.editIndex === null) {
         // Set final point
@@ -51,9 +56,11 @@ export default function AdminEditQuestionController ($scope, $state, $stateParam
         }
 
         $scope.points.push($scope.point)
+        $scope.finalPointHints.push({ value: $scope.point.finalHint })
       } else {
         // If we are editing an existing point
         $scope.points[$scope.editIndex] = $scope.point
+        $scope.finalPointHints[$scope.editIndex] = { value: $scope.point.finalHint }
         $scope.editIndex = null
       }
 
@@ -61,7 +68,9 @@ export default function AdminEditQuestionController ($scope, $state, $stateParam
     }
   }
 
-  // Set given point as final
+  /**
+   * Set given point as final
+   */
   $scope.setFinalPoint = (point) => {
     $scope.points.forEach((e) => {
       e.final = false
@@ -70,21 +79,38 @@ export default function AdminEditQuestionController ($scope, $state, $stateParam
     $scope.points[$scope.points.indexOf(point)].final = true
   }
 
-  // Edit given point
+  /**
+   * Edit given point
+   */
   $scope.editPoint = (point) => {
     $scope.point = point
     $scope.editIndex = $scope.points.indexOf(point)
   }
 
-  // Delete point from the list
+  /**
+   * Delete point from the list
+   */
   $scope.deletePoint = (point) => {
-    $scope.points.splice($scope.points.indexOf(point), 1)
+    let index = $scope.points.indexOf(point)
+    // Remove the points from the question points array
+    $scope.points.splice(index, 1)
+    // Remove the hint from the final point hints array
+    $scope.finalPointHints.splice(index, 1)
   }
 
-  // Save question
+  /**
+   * Save question
+   */
   $scope.submitQuestion = () => {
-    Question.update({ id: $stateParams.id }, { points: $scope.points }, () => {
+    let points = $scope.points.slice()
+    points.forEach((point) => {
+      delete point.finalHint
+    })
+
+    Question.update({ id: $stateParams.id }, { points: points, hints: $scope.finalPointHints }, () => {
       $state.go('admin.questions.all')
+    }, (response) => {
+      $scope.error = response.data
     })
   }
 }
