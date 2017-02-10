7c1b2af5f0fc69ff1a22e4889ba9af00b508f61c
iryu54 <corentinlabroche@gmail.com>
2017-02-10 20:27:19 +0000

    Gestion des limites

--- Client/src/app/game/leaderboard.controller.js
+++ Client/src/app/game/leaderboard.controller.js
@@ -1,11 +1,19 @@
 
 export default function LeaderBoard ($scope, Score) {
   $scope.offset = 0
-  $scope.limit = 3
-  Score.pagination({offset: $scope.offset, limit: $scope.limit}, function (data) {
+  $scope.currentLimit = 5
+  $scope.limits = {3: 3, 5: 5, 10: 10, 15: 15, 20: 20, 50: 50}
+  Score.pagination({offset: $scope.offset, limit: $scope.currentLimit}, function (data) {
     console.log(data)
     $scope.scores = data
   })
+  $scope.limitChanged = (newLimit) => {
+    $scope.currentLimit = newLimit
+    Score.pagination({offset: $scope.offset, limit: $scope.currentLimit}, function (data) {
+      console.log(data)
+      $scope.scores = data
+    })
+  }
 }
 
 LeaderBoard.$inject = ['$scope', 'Score']
